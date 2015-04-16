87348b2ece2136326c17429313aa4e9aa0b95245
lookitsash <lookitsash@gmail.com>
2015-04-16 09:34:56 -0400

    Project template

---
+++ BiddingApp/BasePage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Foundation;
+
+namespace BiddingApp
+{
+    public class BasePage : SmartPage
+    {
+        protected override Logger GetLogger(string name)
+        {
+            return Statics.GetLogger(name);
+        }
+    }
+}
---
+++ BiddingApp/Statics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Foundation;
+
+namespace BiddingApp
+{
+    public static class Statics
+    {
+        static Statics()
+        {
+            Logger.SetDefaultFilePathFormat(LogDirectory);
+        }
+
+        public static ConnectionStringAdapter GetSqlConnectionStringAdapter() { return ConfigAdapter.GetConnectionStringAdapter("SqlConnectionString").SetApplicationName(ConfigAdapter.GetConnectionStringAdapter("SqlConnectionString").SqlConnectionStringBuilder.ApplicationName + " [" + Environment.MachineName + "]" + (GeneralUtil.IsVisualStudioMode ? " (Visual Studio)" : String.Empty)); }
+        public static string SqlConnectionString { get { return GetSqlConnectionStringAdapter().SqlConnectionString; } }
+
+        public static string LogDirectory { get { return ConfigAdapter.GetAppSetting("LogDirectory"); } }
+
+        public static Logger GetLogger() { return GetLogger(String.Empty); }
+        public static Logger GetLogger(string name)
+        {
+            return new Logger(name);
+        }
+    }
+}
